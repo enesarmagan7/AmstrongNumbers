bcaaeced6f8a708b8c249942c905cdf7780edd45
enesarmagan7 <144248210+enesarmagan7@users.noreply.github.com>
2023-09-28 13:47:53 +0300

    Add files via upload

---
+++ AmstrongNumbers.java
@@ -0,0 +1,47 @@
+package Letcode;
+
+import java.util.Scanner;
+
+public class AmstrongNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz :");
+        int number = input.nextInt();
+        int basNumber = 0;
+        int tempNumber = number;
+        int basValue;
+        int result = 0;
+        int basPow;
+        int total=0;
+        while (tempNumber != 0) {
+            tempNumber /= 10;
+            basNumber++;
+        }
+
+        tempNumber = number;
+        while (tempNumber != 0) {
+            basValue = tempNumber % 10;
+            // 1*1*1*1 = 1^4
+            basPow = 1;
+            for (int i = 1; i <= basNumber; i++) {
+                basPow *= basValue;
+            }
+            result += basPow;
+            tempNumber /= 10;
+        }
+        tempNumber=number;
+        if (result == number) {
+            System.out.println(number + " sayısı bir Armstrong sayıdır.");
+        } else {
+            System.out.println(number + " sayısı bir Armstrong sayısı değildir.");
+        }
+
+
+
+        while(tempNumber!=0){
+            total+=tempNumber%10;
+            tempNumber=tempNumber/10;
+        }
+        System.out.println("Basamakların toplamı : "+ total);
+    }
+}
